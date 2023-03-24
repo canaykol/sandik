5ab8ca5fa6c94cd57bd971b2ed7b842a790cc89c
canaykol <can.aykol@gmail.com>
2023-03-24 21:43:02 +0300

    first commit

---
+++ src/main/java/com/can/sandik/SandikApplication.java
@@ -0,0 +1,13 @@
+package com.can.sandik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/can/sandik/SandikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.can.sandik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
